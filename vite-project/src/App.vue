bd8b1354f21b3d805aad038192cda7dd4f443bee
1940454106 <1940454106@qq.com>
2022-11-23 18:45:58 +0800

    Master

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -2,12 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Container from './components/container.vue';
+import Test from './components/test/test-rel.vue'
 
 </script>
 
 <template>
-  123
-  <Container></Container>
+  <Test></Test>
+  <!-- <Container></Container> -->
 </template>
 
 <script>
